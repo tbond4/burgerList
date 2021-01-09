db2a417edab094ca604a4280d758fcd16aaa02a0
Tristan Bond <tristanebond@gmail.com>
2021-01-08 17:42:23 -0800

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name,devoured) VALUES("CheeseBurger",false);
+INSERT INTO burgers(burger_name,devoured) VALUES("Baconator",false);
+INSERT INTO burgers(burger_name,devoured) VALUES("Big Mac", true);
