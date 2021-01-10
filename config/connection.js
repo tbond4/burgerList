8da5497280957d7c3f6c3ac5d439c8a4d4f19f1e
Tristan Bond <tristanebond@gmail.com>
2021-01-10 09:32:54 -0800

    connection file

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,19 @@
+const mysql=require("mysql");
+const { threadId } = require("../../../week 13/Day-03/16-MvcExample/config/connection");
 
-//export connection
-module.exports
+const connection = mysql.createConnection({
+    host:"localhost",
+    port:3306,
+    user:"root",
+    password:"cheetah4",
+    database:"burgers_db"
+});
+
+connection.connect(function(err){
+    if(err){
+        console.log("Error connecting" + err.stack);
+        return;
+    }
+    console.log("Connected as id " + threadId);
+});
+module.exports = connection;
